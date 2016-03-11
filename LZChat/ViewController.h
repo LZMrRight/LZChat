86782e1d2fcddb6807816f92b4f4a532bac779e1
LZMrRight <275068651@qq.com>
2016-03-11 12:49:52 +0800

    Initial Commit

---
+++ LZChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZChat
+//
+//  Created by Mr.Right on 16/3/11.
+//  Copyright © 2016年 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LZChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LZChat
+//
+//  Created by Mr.Right on 16/3/11.
+//  Copyright © 2016年 lizheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
